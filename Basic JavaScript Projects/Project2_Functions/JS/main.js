5e10a61557d671215933cf3d08e06b2db248e9bb
Micah Gooden <micah734@me.com>
2021-12-08 13:39:53 -0500

    updated JS and Added Project2

---
+++ Basic JavaScript Projects/Project2_Functions/JS/main.js
@@ -0,0 +1,11 @@
+function myFunction() {
+    var str = "This text is blue"; //This shows up the text
+    var result = str.fontcolor("blue"); //This  makes the above text blue
+    document.getElementById("Blue_Text").innerHTML = result;
+}
+
+function myFunction2() {
+    var sentence = "I am learning";
+    sentence += " a lot from this book!"; //This adds the following sentence with this sentence
+    document.getElementById("Concatenate").innerHTML = sentence;
+}
