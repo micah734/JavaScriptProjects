61888acc6379ae559f8e792268e197121d3a012d
Micah Gooden <micah734@me.com>
2021-12-08 11:18:31 -0500

    updating and adding first assignment

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,10 @@
+window.alert("This is an alert box!"); //making a popup alert box
+var b = "This is a string";
+var c = " you should be able to add this together.";
+document.write(b + c); //concatanate two strings
+var mom = "Megan", dad = "Micah", baby = "W"; //assigning variables
+var mom = mom.fontcolor("green"), dad = dad.fontcolor("red"), baby = baby.fontcolor("orange"); //adding color to each
+document.write(dad);//printing dad
+document.write(9 * 9);//printing an expression
+document.getElementById("mom").innerHTML = result;//putting the value of the result into HTML element with "mom" id
+
